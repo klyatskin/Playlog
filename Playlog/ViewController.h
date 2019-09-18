6497d97865a189ecd09e59a8ebfa26316430bee1
Konstantin Klyatskin <klyatskin@gmail.com>
2019-09-17 21:29:57 -0400

    Initial Commit

---
+++ Playlog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Playlog
+//
+//  Created by Konstantin Klyatskin on 2019-09-17.
+//  Copyright © 2019 Konstantin Klyatskin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
